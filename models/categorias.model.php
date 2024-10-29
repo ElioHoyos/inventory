bcf28b76f55db067bdf22d696266027703595c1d
ElioHoyos <33575595+ElioHoyos@users.noreply.github.com>
2024-10-29 08:18:20 -0500

    categoriaModel --avance

---
+++ models/categorias.model.php
@@ -0,0 +1,33 @@
+<?php
+//invocar o llamar a la conexion
+require_once "conexion.php";
+class ModeloCategoria{
+    // Crear o registrar categoria
+    public static function mdlIngresarCategoria($tabla, $datos){
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla(categoria) VALUES(:categoria)");
+        $stmt->bindParam(":categoria", $datos, PDO::PARAM_STR);
+        if($stmt->execute()){
+            return "ok";
+        }else{
+            return "error";
+        }
+        $stmt->close();
+        $stmt = null;
+    }
+
+    // Mostrar las categorias
+    public static function mdlMostrarCategoria($tabla, $item, $valor){
+        if($item != null){
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+            $stmt->bindParam(":".$item,$valor, PDO::PARAM_STR);
+            $stmt -> execute();
+            return $stmt->fetch();
+        }else{
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+            $stmt -> execute();
+            return $stmt->fetchAll();
+        }
+        $stmt -> close();
+        $stmt = null;
+    }
+}
