d7d78da4dae1a43a2485e0d9d7119fbfa8c369c5
ElioHoyos <33575595+ElioHoyos@users.noreply.github.com>
2024-11-11 10:20:46 -0500

    producto

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -145,14 +145,8 @@ CUERPO DOCUMENTO
     if(isset($_GET["ruta"])){
 
       if($_GET["ruta"] == "inicio" ||
-         $_GET["ruta"] == "usuarios" ||
          $_GET["ruta"] == "categorias" ||
          $_GET["ruta"] == "productos" ||
-         $_GET["ruta"] == "clientes" ||
-         $_GET["ruta"] == "ventas" ||
-         $_GET["ruta"] == "crear-venta" ||
-         $_GET["ruta"] == "editar-venta" ||
-         $_GET["ruta"] == "reportes" ||
          $_GET["ruta"] == "salir"){
 
         include "modulos/".$_GET["ruta"].".php";
